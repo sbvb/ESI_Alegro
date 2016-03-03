854f07cd2d66d54badacd482a6dcf2d55879c00b
sbVB <sbvillasboas@gmail.com>
2016-03-03 15:44:06 -0300

    new style

---
+++ build.sh
@@ -0,0 +1,2 @@
+g++ SourceFiles/main.cpp SourceFiles/Objects.cpp SourceFiles/Sound.cpp SourceFiles/Directory.cpp -oESI  -lallegro_image -lallegro_font -lallegro_ttf -lallegro_primitives -lallegro -lallegro_audio -lallegro_acodec
+
